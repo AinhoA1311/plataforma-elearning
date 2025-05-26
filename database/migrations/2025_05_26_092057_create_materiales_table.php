d4d4b773aafe99055209304ffc25490f0a7edbbc
Ainhoa Rosales <ainhoa@example.com>
2025-05-26 14:10:57 +0200

    Migración creada: materiales

--- database/migrations/2025_05_26_092057_create_materiales_table.php
+++ database/migrations/2025_05_26_092057_create_materiales_table.php
@@ -4,16 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMaterialesTable extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('materiales', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
             $table->string('archivo')->nullable();
-            $table->foreignId('curso_id')->constrained('cursos')->onDelete('cascade');
+            $table->unsignedBigInteger('curso_id');
             $table->timestamps();
+
+            $table->foreign('curso_id')->references('id')->on('cursos')->onDelete('cascade');
         });
     }
 
@@ -21,4 +22,4 @@ class CreateMaterialesTable extends Migration
     {
         Schema::dropIfExists('materiales');
     }
-}
+};
